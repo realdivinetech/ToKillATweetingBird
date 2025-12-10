b0382be02570efcc9ffad9ba7ba7e18f322d7358
Divine Tech <71929014+realdivinetech@users.noreply.github.com>
2025-12-10 13:47:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ToKillATweetingBird",   # The package name
-    version="0.1.0",              # Any version number you choose
-    packages=find_packages(),     # Automatically find all subpackages
-    install_requires=[            # Optional: dependencies
+    name="realdivinetech",          # package name
+    version="0.1.0",                # version number
+    packages=find_packages(where="."),  # auto-discover packages
+    install_requires=[
+        "selenium>=4.8.0",
+        "pandas",
+        "dynaconf",
+        "psycopg2-binary",
+        "lxml",
+        "fake-useragent~=1.2.1",
         "beautifulsoup4",
-        "lxml"
     ],
+    python_requires=">=3.8",        # adjust if needed
 )
